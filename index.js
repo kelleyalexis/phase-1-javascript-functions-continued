2c20b292f84b951c9e90c4398f8990ae04d71ad8
Kelley Shackelford <kelleyalexis@yahoo.com>
2024-03-08 13:10:16 -0500

    functions lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(message = "go to the office") {
+    return `This Monday, I will ${message}.`
+}
+
+function wrapAdjective(highlight) {
+  return function(adjective) {
+    return `You are ${highlight}${adjective}${highlight}!`;
+  }
+}
+
+
+
+
+
